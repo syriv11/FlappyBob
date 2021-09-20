62a42efeeb17339b3fe67367dcdf8a4470389bff
Bob <robert-3011@mail.ru>
2021-09-20 03:34:46 +0300

    Project setup, also added basic functionality

---
+++ Assets/Main/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerPosition;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = gameObject.transform.position - _playerPosition.position;
+        //gameObject.transform.position
+    }
+
+    private void Update()
+    {
+
+        gameObject.transform.position = new Vector3(_playerPosition.position.x + _offset.x, _offset.y, _offset.z);
+        //gameObject.transform.position = _offset;
+        //gameObject.transform.Translate(new Vector3(_playerPosition.position.x, 0, 0));
+        //gameObject.transform.position = new ;
+    }
+}
---
+++ Assets/Main/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Action SpacePressed;
+
+    private void Update()
+    {   
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpacePressed.Invoke();
+        }
+    }
+}
